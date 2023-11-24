d078e9d0cb6fbba56ea22b87d03a7a9fae2b963c
tenzinkunkyab <tenzinkunkyab5@gmail.com>
2023-11-23 22:11:45 -0500

    made background changer app

--- background-changer/src/App.jsx
+++ background-changer/src/App.jsx
@@ -1,13 +1,34 @@
 import { useState } from 'react'
-
 import './App.css'
+import './index.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [color,setColor]=useState('olive');
+const handleClick = ()=>{
+  setColor('green')
+}
+
 
   return (
     <>
     
+    
+     <div className='container' style={{backgroundColor:color}}>
+     
+      <div className='button'>
+      <h1>Background Changer App</h1>
+      <button className='btn' onClick={()=>setColor('red')} style={{backgroundColor:'red'}}>Red</button>
+      <button className='btn' onClick={()=>setColor('blue')} style={{backgroundColor:'blue'}}>blue</button>
+      <button className='btn' onClick={()=>setColor('green')} style={{backgroundColor:'green'}}>green</button>
+      <button className='btn'onClick={()=>setColor('yellow')} style={{backgroundColor:'yellow'}}>yellow</button>
+      <button className='btn' onClick={()=>setColor('brown')} style={{backgroundColor:'brown'}}>brown</button>
+      <button className='btn'onClick={()=>setColor('black')} style={{backgroundColor:'black'}}>black</button>
+      
+
+      </div>
+     </div>
     </>
   )
 }
